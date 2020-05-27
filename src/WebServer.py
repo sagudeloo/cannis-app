abdc478d05071a00dbbd7b851f44993192872650
sagudeloo <sagudeloo@eafit.edu.co>
2020-05-27 01:22:30 -0500

    version con funcionalidad parcial del servidor

--- src/WebServer.py
+++ src/WebServer.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-
 #Instancia de flask
 app = Flask(__name__)
 
@@ -9,39 +8,38 @@ def index():
     return render_template('index.html')
 
 #Ruta para annadir un nuevo objeto
-@app.route('/anadirObjeto', methods=['POST', 'GET'])
+@app.route('/anadirObjeto', methods=['POST'])
 def anadirObjeto():
     if request.method == 'POST':
-        print(request.form)
-        """ data={
-            'object':{
-                'descripcion': request.form['descripcion'],
-                'color': request.form['color'],
-                'estado': 1,
-                'localizacionEncontrado':  request.form['localizacionEncontrado'],
-                'foto': request.form[foto]
-            },
-            'persona':{
-                'documento': request.form['documentoPersona'],
-                'nombre': request.form['nombrePersona'],
-                'telefono': request.form['telefonoPersona'],
-            },
-            'registro':{
-                'fecha': request.form['fecha'],
-                'usuario': request.form['usuario'],
-                'accion' : 'ingreso'
-            }
+        print(request.form, 'eureca')
+        """object={
+            'diaEncontrado': request.form['diaEncontrado'],
+            'color': request.form['color'],
+            'estado': 1,
+            'localizacionEncontrado':  request.form['localizacionEncontrado'],
+            'foto': request.form[foto],
+            'descripcion': request.form['descripcion']
         }
-        print(data) """
+        persona={
+            'documento': request.form['documentoPersona'],
+            'nombre': request.form['nombrePersona'],
+            'telefono': request.form['telefonoPersona'],
+        }
+        registro={
+            'fecha': request.form['fecha'],
+            'usuario': request.form['usuario'],
+            'documento': request.form['documentoPersona'],
+            'accion' : 'ingreso'
+        }"""
     return render_template('nuevoObjeto.html')
 
 #Ruta para realizar la busqueda de objetos
-@app.route('/buscarObjetos', methods=['POST', 'GET'])
+@app.route('/buscarObjeto', methods=['POST'])
 def buscarObjetos():
     if request.method == 'POST':
         print(request.form)
         print("Se va a buscar un objeto")
-    return render_template('buscarObjetos.html')
+    return render_template('buscarObjeto.html')
 
 """ @app.route('/login')
 def login():
